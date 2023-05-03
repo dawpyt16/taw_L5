78614b7ff971d7345b0a9f3f6c866be9a41f492e
Dawid Pytka <62145751+dawpyt16@users.noreply.github.com>
2023-05-03 13:13:10 +0200

    taw_l8

---
+++ rectangle.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Rectangle = exports.Point = void 0;
+var Point = /** @class */ (function () {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Point.prototype.move = function (dx, dy) {
+        this.x += dx;
+        this.y += dy;
+    };
+    return Point;
+}());
+exports.Point = Point;
+var Rectangle = /** @class */ (function () {
+    function Rectangle(p1, p2, p3, p4) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.p4 = p4;
+    }
+    Rectangle.prototype.move = function (dx, dy) {
+        this.p1.move(dx, dy);
+        this.p2.move(dx, dy);
+        this.p3.move(dx, dy);
+        this.p4.move(dx, dy);
+    };
+    Rectangle.prototype.getArea = function () {
+        var width = Math.sqrt(Math.pow(this.p2.x - this.p1.x, 2) + Math.pow(this.p2.y - this.p1.y, 2));
+        var height = Math.sqrt(Math.pow(this.p4.x - this.p1.x, 2) + Math.pow(this.p4.y - this.p1.y, 2));
+        return width * height;
+    };
+    return Rectangle;
+}());
+exports.Rectangle = Rectangle;
